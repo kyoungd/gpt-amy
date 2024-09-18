9c370f641123c0218a5745b4fcb8ecebcd31c620
kyoungd <kyoungd@hotmail.com>
2024-09-18 12:29:36 -0700

    fixed the horizontal screen menu drop issue

--- src/components/NavBar/sub-component/MobileNavMenu.jsx
+++ src/components/NavBar/sub-component/MobileNavMenu.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {
+    getClosest,
+    getSiblings,
+    slideToggle,
+    slideUp,
+} from "../../../utils";
 import { useUserState } from '../../UserContext';
 
 const MobileNavMenu = () => {
     const { isAuthenticated } = useUserState();
     const homeUrl = process.env.PUBLIC_URL + "/" + (isAuthenticated ? "dashboard" : "");
+    const onClickHandler = (e) => {
+        const target = e.currentTarget;
+        const parentEl = target.parentElement;
+
+        if (parentEl?.classList.contains("menu-toggle") || target.classList.contains("menu-toggle")) {
+            const element = target.classList.contains("icon") ? parentEl : target;
+            const parent = getClosest(element, "li");
+            const childNodes = parent.childNodes;
+            const parentSiblings = getSiblings(parent);
+
+            // Close all sibling submenus
+            parentSiblings.forEach((sibling) => {
+                const sibChildNodes = sibling.childNodes;
+                sibChildNodes.forEach((child) => {
+                    if (child.nodeName === "UL") {
+                        slideUp(child, 1000);
+                    }
+                });
+            });
+
+            // Toggle the clicked submenu
+            childNodes.forEach((child) => {
+                if (child.nodeName === "UL") {
+                    slideToggle(child, 1000);
+                }
+            });
+        }
+    };
+
     return (
         <nav className="site-mobile-menu">
             <ul>
@@ -13,7 +48,7 @@ const MobileNavMenu = () => {
                 </li>
                 <li className="has-children">
                     <NavLink><span className="menu-text">AI</span></NavLink>
-                    <span className="menu-toggle"><i className="far fa-angle-down"></i></span>
+                    <span className="menu-toggle" onClick={onClickHandler}><i className="far fa-angle-down"></i></span>
                     <ul className="sub-menu">
                         <li><NavLink to="/ai/demo-appointment"><span className="menu-text">Appointment AI</span></NavLink></li>
                         <li><NavLink to="/ai/demo-car-part"><span className="menu-text">Car Parts AI</span></NavLink></li>
